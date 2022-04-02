9fcac35f56a105fb1f428b161c627b75baa57a53
greenjetta <90808861+greenjetta@users.noreply.github.com>
2022-04-02 11:26:26 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
 import random
 
-def play():
-    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors: \n")
-    computer = random.choice(['r', 'p', 's'])
+def guess(x):
+    random_number = random.randint(1, x)
+    guess = 0
+    while guess != random_number:
+        guess = int(input(f'Guess a number between 1 and {x}: '))
+        if guess < random_number:
+            print('sorry too low.')
+        elif guess > random_number:
+            print('sorry too high')
 
-    if user == computer:
-        return 'It\'s a tie'
+    print('yay u got the random number {random_number} correct! ')
 
-    # r > s, s > p, p > r
-    if is_win(user, computer):
-        return 'You won!'
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else: guess = low # could also be high b/c low = high
+        guess = random.randint(low, high)
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?? ').lower()
+        if feedback == 'h':
+            high = guess - 1
+        if feedback == 'l':
+            low = guess + 1
 
-    return 'You lost!'
-
-def is_win(player, opponent):
-    # return true if player wins
-    # r > s, s > p, p > r
-    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
-        or (player == 'p' and opponent == 'r'):
-        return True
-
-print(play())
-
-
-
+    print(f'yay! the computer guessed your number, {guess}, correctly!')
 
 
+computer_guess(1000)
